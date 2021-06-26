17e1768b9acfb4d84c927c73671d252c114de928
bbhupen <67063163+bbhupen@users.noreply.github.com>
2021-06-27 00:40:13 +0530

    Files Updated

--- code/form.js
+++ code/form.js
@@ -1,5 +1,10 @@
 form = document.getElementById("form")
 form.addEventListener('submit', submitForm);
+var imgName, imgUrl;
+var files = [];
+var reader = new FileReader();
+
+
 
 
 // Your web app's Firebase configuration
@@ -15,8 +20,25 @@ appId: "1:295703687700:web:1c4ea7ef6b2e3762f3ad01"
 firebase.initializeApp(firebaseConfig);
 
 var submitRef = firebase.database()
+var fireStorage = firebase.storage()
 
 //Submit Form
+
+document.getElementById("select").onclick = function (e) {
+    var input = document.createElement('input');
+    input.type = 'file';
+
+    input.onchange = e => {
+        files = e.target.files;
+        reader = new FileReader();
+        reader.onload = function () {
+            document.getElementById("myimg").src = reader.result;
+        }
+        reader.readAsDataURL(files[0])
+    }
+    input.click();
+}
+
 
 function submitForm(e) {
     e.preventDefault();
@@ -25,11 +47,16 @@ function submitForm(e) {
     var link = getInputVal("link");
     var desc = getInputVal("desc");
     var category = getInputVal("category")
+    var image = getInputVal("namebox")
+
+    
 
     console.log(email)
 
-    saveSubmission(email, title, link, desc, category);
+    //Text Uploads
+    // saveSubmission(email, title, link, desc, category, image);
 
+    uploadImage(email, title, link, desc, category, image)
     document.querySelector('.alert').style.display = 'block'
 
     setTimeout(function () {
@@ -45,16 +72,52 @@ function submitForm(e) {
 
 
 
-function saveSubmission(email, title, link, desc, category) {
+function uploadImage(email, title, link, desc, category, image) {
+    var uploadTask = fireStorage.ref(category+'/'+image+".png").put
+    (files[0])
+
+    uploadTask.on('state_changed', function (snapshot) {
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        document.getElementById('UpProgress').innerHTML = 'Upload' + progress + '%';
+    },
+    
+        function (error) {
+        alert('error')
+        },
+
+        function () {
+            uploadTask.snapshot.ref.getDownloadURL().then(function (url) {
+                image = url;
+
+                var newSubmitRef = submitRef.ref(category).push();
+                newSubmitRef.set({
+                    email: email,
+                    title: title,
+                    link: link,
+                    desc: desc,
+                    image: url
+                })
+                console.log('image added successfully')
+            })
+        })
+    
+    return imgUrl
+}
+
+
+
+function saveSubmission(email, title, link, desc, category, image) {
     var newSubmitRef = submitRef.ref(category).push();
     newSubmitRef.set({
-        email: email, 
+        email: email,
         title: title,
         link: link,
-        desc: desc
+        desc: desc,
+        image: image
     })
 }
 
 function getInputVal(id) {
     return document.getElementById(id).value;
 }
+
--- code/index.js
+++ code/index.js
@@ -10,10 +10,14 @@ var firebaseConfig = {
     messagingSenderId: "295703687700",
     appId: "1:295703687700:web:1c4ea7ef6b2e3762f3ad01"
 };
+
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 var getDataRef = firebase.database()
+var storage = firebase.storage()
+
 
 
 
@@ -31,6 +35,7 @@ document.getElementById('category').addEventListener('change', function () {
     var category = getInputVal("category")
     getDataRef.ref(category).once("value", function (snapshot) {
         snapshot.forEach(function (element) {
+            clearBox("toor")
             document.querySelector('#root').innerHTML += `
         <div class="card mb-3">
             <img class="card-img-top" src="{src}" id="imageSRC">
@@ -49,7 +54,7 @@ document.getElementById('category').addEventListener('change', function () {
 
 
 function clearBox(elementID) {
-    document.getElementById(elementID).innerHTML = "";
+    document.getElementById(elementID).innerHTML ="";
 }
 
 
