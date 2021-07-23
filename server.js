f8789aeb5a9aec8f1633c3c10f53c01b0395fc0a
bbhupen <bbhupen601@gmail.com>
2021-07-23 10:57:28 +0530

    Profile add

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ const express = require('express');
 const app = express();
 app.use(requireHTTPS);
 
-app.use(express.static("./dist/dev-ess"));
+app.use(express.static("./dist/devEss"));
 
 
 app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/dev-ess/'}
+    res.sendFile('index.html', {root: 'dist/devEss/'}
   );
   });
 
